118b4eb3759e6a149ef55ce9217ea47fdd32b4f1
Albert Hilazo <albert.hilazo@capside.com>
2016-02-10 12:04:34 +0100

    Updated spawn_worker to use Module::Runtime and MooseX::Getopt

--- bin/spawn_worker.pl
+++ bin/spawn_worker.pl
@@ -2,21 +2,29 @@
 use strict;
 use warnings;
 
-my $queue  = $ENV{SQS_TEST_QUEUE};
-my $region = $ENV{SQS_TEST_REGION};
+use Module::Runtime qw/require_module/;
 
-my ($worker) = @ARGV;
 
-eval("use $worker");
-if ($@) {
-  print 'Error loading worker: '.$worker;
-  die $@;
+package SpawnWorkerArgs {
+  use Moose;
+  with 'MooseX::Getopt';
+
+  has 'worker'    => (is => 'ro', isa => 'Str', required => 1, documentation => 'The SQS::Worker class name');
+  has 'queue_url' => (is => 'ro', isa => 'Str', required => 1, documentation => 'The SQS queue URL to poll for messages');
+  has 'region'    => (is => 'ro', isa => 'Str', required => 1, documentation => 'The SQS region identifier');
 }
 
+
+my $args = SpawnWorkerArgs->new_with_options();
+
+require_module($args->worker);
+
 # Instance worker
-my $worker_instance = $worker->new(
-  queue_url => $queue,
-  region    => $region
+my $worker_instance = $args->worker->new(
+  queue_url => $args->queue_url,
+  region    => $args->region
 );
 
-$worker_instance->run();
+if ($worker_instance->does('SQS::Worker')) {
+  $worker_instance->run();
+}
